e4be6542820f0b38f945e5d49ebb1a81a70fb222
Dheeraj Yennam <dyennam@gmail.com>
2014-10-10 12:15:22 -0700

    Initial Upload of app
    Uploaded a page view based initial folder

---
+++ Dr_U_App/Dr_U_App/RootViewController.designer.cs
@@ -0,0 +1,21 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace Dr_U_App
+{
+	[Register ("RootViewController")]
+	partial class RootViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
