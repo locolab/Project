05b5b09aa0bc6f24dab3d235d25505d2d10271e7
Dheeraj Yennam <dyennam@gmail.com>
2014-10-17 10:17:50 -0700

    Made basic Buttons
    Added next / prev buttons and label

--- Dr_U_App/Dr_U_App/RootViewController.designer.cs
+++ Dr_U_App/Dr_U_App/RootViewController.designer.cs
@@ -1,12 +1,10 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
-using System;
 using MonoTouch.Foundation;
-using MonoTouch.UIKit;
 using System.CodeDom.Compiler;
 
 namespace Dr_U_App
@@ -14,8 +12,31 @@ namespace Dr_U_App
 	[Register ("RootViewController")]
 	partial class RootViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton buttonNext { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton buttonPrev { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel labelTitle { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (buttonPrev != null) {
+				buttonPrev.Dispose ();
+				buttonPrev = null;
+			}
+
+			if (buttonNext != null) {
+				buttonNext.Dispose ();
+				buttonNext = null;
+			}
+
+			if (labelTitle != null) {
+				labelTitle.Dispose ();
+				labelTitle = null;
+			}
 		}
 	}
 }
