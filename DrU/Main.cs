ee3ee9d343ee99e1ff06fc1dd730a996933dfc0b
Tyler Wallace <tylerw710@gmail.com>
2014-10-31 11:23:51 -0700

    Updates to GUI
    Basic GUI updates. Should be able to work with Ask button and addition
    to the text area location. Need to add images and button functionalities

--- DrU/Main.cs
+++ DrU/Main.cs
@@ -14,7 +14,6 @@ namespace DrU
 		{
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
-
 			UIApplication.Main (args, null, "AppDelegate");
 		}
 	}
