15e406a467e359e75b0ebebeeb678d2319be62a8
Dheeraj Yennam <dyennam@gmail.com>
2014-11-14 23:51:12 -0700

    Added Login Screen (With working transition)

---
+++ DrU/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  DrU
+//
+//  Created by Dheeraj Yennam on 11/14/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ DrU/obj/Xcode/8/DerivedData/DrU/Build/Intermediates/DrU.build/MonoTouch-iphonesimulator/DrU.build/Objects-normal/i386/DrUViewController-EBC79F9EC3B37FDF.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Dheeraj_Mac/Github/Project/DrU/obj/Xcode/8/DrUViewController.m \
+  /Users/Dheeraj_Mac/Github/Project/DrU/obj/Xcode/8/DrUViewController.h
