7f1271c0170e34563d4c4314ad1471e32f2a5077
Dheeraj Yennam <dyennam@gmail.com>
2014-11-15 00:35:34 -0700

    Working Login screen Fixed image errors

--- DrU/LoginViewController.designer.cs
+++ DrU/LoginViewController.designer.cs
@@ -1,10 +1,12 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
+using System;
 using MonoTouch.Foundation;
+using MonoTouch.UIKit;
 using System.CodeDom.Compiler;
 
 namespace DrU
@@ -12,9 +14,36 @@ namespace DrU
 	[Register ("LoginViewController")]
 	partial class LoginViewController
 	{
-		
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btn_Go { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView img_LoginBackground { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField txt_Name { get; set; }
+
+		[Action ("btn_Go_TouchUpInside:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn_Go_TouchUpInside (UIButton sender);
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (btn_Go != null) {
+				btn_Go.Dispose ();
+				btn_Go = null;
+			}
+			if (img_LoginBackground != null) {
+				img_LoginBackground.Dispose ();
+				img_LoginBackground = null;
+			}
+			if (txt_Name != null) {
+				txt_Name.Dispose ();
+				txt_Name = null;
+			}
 		}
 	}
 }
