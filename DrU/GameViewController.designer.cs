9651c46705b90c707b16a6e301c05d57c18fdf67
Dheeraj Yennam <dyennam@gmail.com>
2014-11-15 01:49:04 -0700

    Fully working base app no errors as of now
    Game screen has back button implemented with transition

--- DrU/GameViewController.designer.cs
+++ DrU/GameViewController.designer.cs
@@ -1,10 +1,12 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
+using System;
 using MonoTouch.Foundation;
+using MonoTouch.UIKit;
 using System.CodeDom.Compiler;
 
 namespace DrU
@@ -12,9 +14,28 @@ namespace DrU
 	[Register ("GameViewController")]
 	partial class GameViewController
 	{
-		
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btn_GoBackMain { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView img_GameBackground { get; set; }
+
+		[Action ("btn_GoBackMain_TouchUpInside:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn_GoBackMain_TouchUpInside (UIButton sender);
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (btn_GoBackMain != null) {
+				btn_GoBackMain.Dispose ();
+				btn_GoBackMain = null;
+			}
+			if (img_GameBackground != null) {
+				img_GameBackground.Dispose ();
+				img_GameBackground = null;
+			}
 		}
 	}
 }
